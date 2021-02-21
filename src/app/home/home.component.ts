39fe2c8ceb49013a4f4b7dd62c0f6eb39d3dec16
KFischer (MBP) <klaus.fischer@eloware.com>
2021-02-21 19:23:15 +0100

    Testing firebase hosting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Mineral} from '../models/mineral';
-import {createConnection} from 'typeorm';
 
 @Component({
   selector: 'app-home',
@@ -13,19 +11,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    createConnection({
-      type: 'sqlite',
-      database: './Mineralien.sqlite',
-      entities: [
-        Mineral
-      ],
-      synchronize: true,
-      logging: false
-    }).then(connection => {
-      const repository = connection.getRepository(Mineral);
-
-      // here you can start to work with your entities
-    }).catch(error => console.log(error));
   }
 
 }
